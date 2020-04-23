a6daf0ba26033d4c41086e106f4452334d5d9979
wagnerrodrigo <wagnerrodrio.pan@gmail.com>
2020-04-23 16:32:00 -0300

    basic header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text , View} from 'react-native';
+
+export default function Header(){
+    return(
+        <View>
+            <Text>
+                O componente esta funcionado
+            </Text>
+        </View>
+    )
+}
