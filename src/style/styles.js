c81b7bd9e54a67f8e406de11335a6ff9730e3253
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-04-29 16:30:11 -0300

    basic styles component

---
+++ src/style/styles.js
@@ -0,0 +1,11 @@
+import sytled from 'styled-components/native'
+
+export const VieW = sytled.View`
+    background: rgb(34,134,234);
+    margin-top: 25px;
+`;
+
+export const TexT = sytled.Text`
+    font-size: 30px;
+    color: white;
+`;
