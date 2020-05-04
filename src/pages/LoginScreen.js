d485061632836f43e16ea0caf8d61e08201d3908
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-05-03 21:13:20 -0300

    Change teacher code.
    created project in google firebase with name notifica and nickname
    fire-notifica.
    was modified teacher authentication code for my authentication code.

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -15,15 +15,15 @@ export default class LoginScreen extends React.Component {
     
     componentDidMount() {
      var firebaseConfig = {
-        apiKey: "AIzaSyCmbOG0wwMeWfzqZzBYJVHJaoJtQAvTyhU",
-        authDomain: "aulamovelbd.firebaseapp.com",
-        databaseURL: "https://aulamovelbd.firebaseio.com",
-        projectId: "aulamovelbd",
-        storageBucket: "aulamovelbd.appspot.com",
-        messagingSenderId: "815556226593",
-        appId: "1:815556226593:web:ef6c02663cb500dd327d60",
-        measurementId: "G-2F8BJ8760V"
-      };
+       apiKey: "AIzaSyDt3EVOo7XOrY3GHr-qNiP-7YdS5IQHHnQ",
+       authDomain: "notifica-a3d65.firebaseapp.com",
+       databaseURL: "https://notifica-a3d65.firebaseio.com",
+       projectId: "notifica-a3d65",
+       storageBucket: "notifica-a3d65.appspot.com",
+       messagingSenderId: "1083664168744",
+       appId: "1:1083664168744:web:f885364c052c28f086821d",
+       measurementId: "G-CX8L5EZEYJ",
+     };
       // Initialize Firebase
 
     //    if(!firebase.app.length){
