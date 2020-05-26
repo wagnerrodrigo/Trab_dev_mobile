e95e57f0c7f0f2e89843b14e64f7845d551b5d12
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-05-26 17:31:32 -0300

    recovering old files

--- src/style/styles.js
+++ src/style/styles.js
@@ -1,4 +1,5 @@
 import sytled from 'styled-components/native'
+import styled from 'styled-components';
 
 export const VieW = sytled.View`
     background: rgb(34,134,234);
@@ -8,4 +9,16 @@ export const VieW = sytled.View`
 export const TexT = sytled.Text`
     font-size: 30px;
     color: white;
+`;
+
+export const Fab = styled.TouchableOpacity`
+height: 50px;
+width: 50px;
+border-radius: 200px;
+position: absolute;
+bottom: 20px;
+right: 20px;
+justify-content: center;
+align-Items: center;
+background-color: #06f;
 `;
