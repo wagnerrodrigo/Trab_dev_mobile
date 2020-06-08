cd279f0b1e15f78434a045197a1a6a018d56aef4
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-06-07 23:56:35 -0300

    the ContentItem component function has ended

--- src/components/ContentItem.js
+++ src/components/ContentItem.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { ViewItem, LineText,FakeImage } from "../style/styles";
 
-const ContentItem = (props) =>{
-    return(
-        <TouchableOpacity>
-            <Text>ComponenteItem</Text>
-        </TouchableOpacity>
-    )
-}
+
+const ContentItem = (props) => {
+  const { name, img, desc, id } = props;
+
+    function renderImg(img){
+        if(img){
+            return <FakeImage source={{uri: img}} />;
+        } else{
+            return <FakeImage/>
+        }
+    }
+
+
+  return (
+    <TouchableOpacity>
+      <ViewItem>
+        {renderImg(img)}
+        <LineText>{name}</LineText>
+      </ViewItem>
+    </TouchableOpacity>
+  );
+};
 
 export default ContentItem;
