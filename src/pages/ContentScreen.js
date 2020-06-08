26c36941643aff2e896ede1f2420d00b47bc141b
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-06-07 21:38:40 -0300

    built the ContentScreen
    This component will contain the content
    loaded from the database (firebase)

--- src/pages/ContentScreen.js
+++ src/pages/ContentScreen.js
@@ -1,57 +1,67 @@
-import React from 'react';
-import { ActivityIndicator, ScrollView} from 'react-native';
-import Firebase from '../database/Firebase';
-import {View, Text} from 'react-native';
+import React from "react";
+import { ActivityIndicator, ScrollView } from "react-native";
+import firebase from "../database/Firebase";
+import { Text } from "react-native";
+import { Fab, TexT, VieW } from "../style/styles";
+import ContentItem from "../components/ContentItem";
 
+export default class ContentScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: [],
+      isLoading: true,
+    };
+  }
 
-export class ContentScreen extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            contents: [],
-            isLoading: true
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("contents")
+      .onSnapshot((query) => this.contentUpdate(query));
+  }
+  //qualquer nome do metodo
+  contentUpdate(query) {
+    const contents = [];
+    query.forEach((doc) => {
+      const { name, desc, img } = doc.data();
+      contents.push({
+        id: doc.id,
+        name,
+        desc,
+        img,
+      });
+    });
+    this.setState({
+      contents,
+      isLoading: false,
+    });
+  }
 
-        }
+  renderActivityIndicator() {
+    if (this.setState.isLoading) {
+      return (
+        <VieW>
+          <ActivityIndicator size="large" />
+        </VieW>
+      );
     }
+  }
 
-    componentDidMount(){
-        Firebase.firestore().collection('contents').onSnapshot((query)=>this.contentUpdate);
-    }
-        //qualquer nome do metodo
-    contentUpdate(query){
-        const contents = [];
-        query.forEach((doc) =>{
-            const { name , desc , img} = doc.data();
-            contents.push({
-                id: doc.id,
-                name,
-                desc,
-                img
-            })
-        });
-        this.setState({
-            contents,
-            isLoading:false,
-        })
+  render() {
+    this.renderActivityIndicator();
+    const { contents } = this.state;
+    const itens = contents.map((content, index) => <ContentItem/>);
 
-    }
+    return (
+      <VieW>
+        <Text>OS ITENS DO BANCO SERÁ RENDERIZADO ABAIXO</Text>
 
-    renderActivityIndicator(){
-        if(this.setState.isLoading){
-            return(
-                <View>
-                    <ActivityIndicator size="large"/>
-                </View>
-            )
-        }
-    }
-
-    render(){
-
-        { this.renderActivityIndicator()}
-
-        return(
-            <View><Text>OI</Text></View>
-        )
-    }
+        <Fab>
+          <TexT>+</TexT>
+        </Fab>
+        <ScrollView>{itens}</ScrollView>
+      </VieW>
+    );
+  }
 }
