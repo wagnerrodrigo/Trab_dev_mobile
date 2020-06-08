78eefb0295c5d43ac19fe5b83eadb16dbd8b7fb1
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-06-07 21:28:36 -0300

    built the component structure.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import LoginScreen from './src/pages/LoginScreen';
-
+import ContentScreen from './src/pages/ContentScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,19 +12,28 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} 
-          options={{ 
-            title: 'My App',
+        <Stack.Screen
+          name="ContentScreen"
+          component={ContentScreen}
+        />
+
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            title: "My App",
             headerStyle: {
-              backgroundColor: '#3f68d1',
-              borderBottomColor: '#fff',
-              borderBottomWidth: 1
+              backgroundColor: "#3f68d1",
+              borderBottomColor: "#fff",
+              borderBottomWidth: 1,
             },
             headerTitleStyle: {
-              color: '#fff',
-              fontSize: 30
-          }, }}/>
+              color: "#fff",
+              fontSize: 30,
+            },
+          }}
           
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
