ee77d7c7ce33065119302a1c972ade5dbe00693b
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-06-07 21:13:03 -0300

    deleted header component, modified style component
    the header component was removed because
    it was not being used
    
    the styling component has been modified
    to accommodate items better on the screen

--- src/style/styles.js
+++ src/style/styles.js
@@ -1,9 +1,15 @@
-import sytled from 'styled-components/native'
-import styled from 'styled-components';
+import sytled from "styled-components/native";
+import styled from "styled-components";
 
 export const VieW = sytled.View`
-    background: rgb(34,134,234);
-    margin-top: 25px;
+   background: #ffffff;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    margin-top: ${(props) => props.margintop || 0};
+    margin-left: ${(props) => props.marginleft || 0};
+    margin-right: ${(props) => props.marginright || 0};
+    margin-bottom: ${(props) => props.marginbottom || 0};
 `;
 
 export const TexT = sytled.Text`
@@ -12,13 +18,13 @@ export const TexT = sytled.Text`
 `;
 
 export const Fab = styled.TouchableOpacity`
-height: 50px;
-width: 50px;
-border-radius: 200px;
-position: absolute;
-bottom: 20px;
-right: 20px;
-justify-content: center;
-align-Items: center;
-background-color: #06f;
+  height: 50px;
+  width: 50px;
+  border-radius: 200px;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  justify-content: center;
+  align-items: center;
+  background-color: #856084;
 `;
