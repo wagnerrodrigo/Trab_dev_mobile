9fc749d0ed8f4a4fb6f07cdd6f899461cd466e20
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-07-02 15:09:39 -0300

    modified component
    modification of the contentScreen component to suit the teacher's

--- src/pages/ContentScreen.js
+++ src/pages/ContentScreen.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { ActivityIndicator, ScrollView } from "react-native";
-import firebase from "../database/Firebase";
-import { Text } from "react-native";
+//import firebase from '../database/Firebase'
+import Sqlite from "../database/Sqlite";
+
 import { Fab, TexT, VieW } from "../style/styles";
 import ContentItem from "../components/ContentItem";
 
-export default class ContentScreen extends React.Component {
+import { useNavigation } from "@react-navigation/native";
+
+const db = new Sqlite();
+let database;
+
+class ContentScreen extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       contents: [],
       isLoading: true,
@@ -15,13 +22,16 @@ export default class ContentScreen extends React.Component {
   }
 
   componentDidMount() {
-    firebase
-      .firestore()
-      .collection("contents")
-      .onSnapshot((query) => this.contentUpdate(query));
+    //       firebase.firestore().collection('contents').onSnapshot((query)=>this.contentUpdate(query));
+    database = db.initDB();
+    const { navigation } = this.props;
+    navigation.addListener("focus", () => {
+      this.contentUpdate();
+    });
   }
-  //qualquer nome do metodo
-  contentUpdate(query) {
+  /*
+    qualquer nome do metodo
+  contentUpdate(query){
     const contents = [];
     query.forEach((doc) => {
       const { name, desc, img } = doc.data();
@@ -29,17 +39,39 @@ export default class ContentScreen extends React.Component {
         id: doc.id,
         name,
         desc,
-        img,
-      });
+        img
+      })
     });
     this.setState({
       contents,
       isLoading: false,
-    });
+    })
+ }
+ */
+
+  contentUpdate() {
+    console.log("contentUpdate");
+    let contents = [];
+
+    db.listContents(database)
+      .then((data) => {
+        console.log("data ", data);
+        contents = data;
+        this.setState({
+          contents,
+          isLoading: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState = {
+          isLoading: false,
+        };
+      });
   }
 
   renderActivityIndicator() {
-    if (this.setState.isLoading) {
+    if (this.state.isLoading) {
       return (
         <VieW>
           <ActivityIndicator size="large" />
@@ -50,23 +82,45 @@ export default class ContentScreen extends React.Component {
 
   render() {
     this.renderActivityIndicator();
+
     const { contents } = this.state;
-    const itens = contents.map((content, index) => (
+    const { navigation } = this.props;
+
+    const items = contents.map((content, index) => (
       <ContentItem
         name={content.name}
         desc={content.desc}
         img={content.img}
         id={content.id}
+        onPress={() =>
+          navigation.navigate("ContentDetailScreen", {
+            id: content.id,
+            desc: content.desc,
+            name: content.name,
+            img: content.img,
+            database: database,
+        })
+      }
       />
     ));
 
     return (
       <VieW>
-        <Fab>
+        <ScrollView>{items}</ScrollView>
+        <Fab
+          onPress={() => {
+            navigation.navigate("SecondScreen", { database: database });
+          }}
+        >
           <TexT>+</TexT>
         </Fab>
-        <ScrollView>{itens}</ScrollView>
       </VieW>
     );
   }
+}
+
+export default function (props) {
+const navigation = useNavigation();
+
+return <ContentScreen {...props} navigation={navigation} />;
 }
