a2335861ee07e9251fbf5699215c9c2e668ca68d
wagnerrodrigo <wagnerrodrigo.pan@gmail.ccom>
2020-07-02 13:57:50 -0300

    loginScreen finalized

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,81 +1,98 @@
-import React from 'react';
-import { View, TextInput, Button } from 'react-native'; 
-import firebase from '../database/Firebase';
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
+//import firebase from 'firebase';
+import firebase from "../database/Firebase";
+import { useNavigation } from "@react-navigation/native";
 
-export default class LoginScreen extends React.Component {
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      mail: "",
+      password: "",
+    };
+  }
 
-        this.state = {
-            mail: '',
-            password: ''
-        }
-    }
-    
+  componentDidMount() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("wagnerrodrigo.pan@gmail.com", "123456789")
+      .then((user) => {
+        console.log("usuario logado ", user);
+      })
+      .catch((error) => {
+        console.log("erro ", error);
+      })
+      .finally(() => {
+        console.log("terminou");
+      });
+  }
 
+  tryLogin() {
+    console.log("usuario", this.state.mail, " senha ", this.state.password);
+    //destructing
+    const { mail, password } = this.state;
+    const { navigation } = this.props;
+    //promisse
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(mail, password)
+      .then((user) => {
+        console.log("usuario logado ", user);
+        navigation.navigate("ContentScreen");
+        Alert.alert("Usuário logado com sucesso", "", [{ text: "OK" }]);
+      })
+      .catch((error) => {
+        //                console.log("erro ", error)
+        //if (error.code === 'auth/user-not-found')
+      })
+      .finally(() => {
+        console.log("terminou");
+      });
+  }
 
-    componentDidMount() {
-         firebase
-           .auth()
-           .signInWithEmailAndPassword(
-             "wagnerrodrigo.pan@gmail.com",
-             "123456789"
-           )
-           .then((user) => {
-             console.log("usuario logado ", user);
-           })
-           .catch((error) => {
-             console.log("erro ", error);
-           })
-           .finally(() => {
-             console.log("terminou");
-           });
-        
-     }
+  onChangeMail(value) {
+    this.setState({ mail: value });
+  }
 
+  onChangePassword(value) {
+    this.setState({ password: value });
+  }
 
-    tryLogin(){
-        console.log("usuario", this.state.mail," senha ",this.state.password);
-    
-        const { mail, password } = this.state;
-        //promisse
-                firebase.auth().signInWithEmailAndPassword(mail, password)
-                    .then(user => {console.log("usuario logado ", user)})
-                    .catch(error => {console.log("erro ", error)})
-                    .finally(() => {console.log("terminou")})
-    
-    }
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="user@email.com"
+          value={this.state.mail}
+          onChangeText={(value) => this.onChangeMail(value)}
+        />
+        <TextInput
+          placeholder="******"
+          value={this.state.password}
+          secureTextEntry
+          onChangeText={(value) => this.onChangePassword(value)}
+        />
+        <Button
+          color="#856084"
+          title="Entrar"
+          onPress={() => this.tryLogin()}
+        />
+      </View>
+    );
+  }
+}
 
-    onChangeMail(value){
-        this.setState({mail: value});
-    }
+export default function (props) {
+  const navigation = useNavigation();
 
-    onChangePassword(value){
-        this.setState({password: value});
-    }
-
-    render() {
-
-
-        return(
-            <View>
-                <TextInput placeholder="user@email.com"
-                           value={this.state.mail}
-                           onChangeText={(value)=>this.onChangeMail(value)}
-                           
-                           />
-                <TextInput placeholder="******"
-                            value={this.state.password}
-                            secureTextEntry
-                            onChangeText={(value)=>this.onChangePassword(value)}
-                                />
-                <Button 
-                   color="#856084" 
-                   title="Entrar" 
-                   onPress={()=>this.tryLogin()}/>
-            </View>    
-        );
-    }
-
-};
+  return <LoginScreen {...props} navigation={navigation} />;
+}
